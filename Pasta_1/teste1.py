1cd62691cbae2a076d6fc467fa5a2d98627a7e49
RYANLOPESFAC <140861020+RYANLOPESFAC@users.noreply.github.com>
2023-09-08 22:08:49 -0300

    suba

---
+++ Pasta_1/teste1.py
@@ -0,0 +1,15 @@
+#git init é apenas uma vez só. 
+#git add . pra adicinar os arquivos no repositorio. 
+#git commit -a -m tem o signiufocado de atualizar as informações. " vai tirar uma fotografia. 
+# (continua aqui..) ele vai rastrear as mudanças que vc fez no seus repositorios "
+# pode trabalhar em outro a paralelo ao mesmo tempo vc pode dar um nome pra ele e ficar alternado de um para outro 
+
+
+#ACESSO REMOTO A DE CASA PRA UNIVALE GITHUB PRA SUBIR OS ARQUIVOS 
+
+#laçoes de repetição 
+#git 
+#kwarts 
+#git ignore 
+#if __name__
+#orientado objeto 
---
+++ Pasta_1/teste2.py
@@ -0,0 +1,22 @@
+primeiro = float(input(f"escreva o primeiro ativo:\n")) # \n seve para quebrar a linha 
+segundo = float(input(f"escreva o primeiro ativo:\n")) 
+
+def maior_valor(primeiro, segundo):
+
+
+    if __name__ == "__main__":
+        print("Univale -revisão de milhões ")
+
+    if primeiro > segundo:
+        print("O maior valor é ",primeiro) 
+        print(f"Primeiro valor é r maior que segundo por {primeiro - segundo}")
+
+    elif segundo > primeiro:
+        print("O maior valor é ",segundo) 
+        print(f"Primeiro valor é maior que segundo por {segundo - primeiro}")
+
+    else: 
+        print("Os valores são iguais")
+
+
+maior_valor(primeiro, segundo)
